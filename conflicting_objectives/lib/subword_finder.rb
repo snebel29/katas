6accfd454612cd5538c7340334aa25a38cca0b49
snebel29 <nebel.sven@gmail.com>
2016-05-19 19:26:17 +0100

    Super fast subword finder

--- conflicting_objectives/lib/subword_finder.rb
+++ conflicting_objectives/lib/subword_finder.rb
@@ -1,9 +1,42 @@
 class SubWordFinder
 
+  @@word_length = 6
   attr_reader :words
+
   def load_file(file, options = {})
-    @words = File.readlines(file, options) { |w| w.chomp }
-    puts @words
+    @all_words = File.readlines(file, options).map {|w| w.strip}.reject { |w| w.empty? }
+    @words = @all_words.select { |w| w.length == @@word_length }
+    @sub_words = @all_words.select { |w| w.length < @@word_length }
+    @sub_words_hash = {}
+    @sub_words.each {|w| @sub_words_hash[w] ||= nil }
+  end
+
+  def find
+    result = {}
+
+    @words.each do |word|
+
+      result[word] = []
+      lbound = 0
+      ubound = word.length - 1
+      counter = 0
+
+      while counter < ubound do
+        part1 = word[lbound..(counter)]
+        part2 = word[(counter + 1)..ubound]
+
+        #puts "Working with #{part1}|#{part2}"
+        if @sub_words_hash.include?(part1) && @sub_words_hash.include?(part2)
+          #puts "There is a match for [#{word}] with [#{part1}] + [#{part2}]"
+          result[word] << [part1, part2]
+        end
+        counter += 1
+
+      end
+
+    end
+
+    result
   end
 
 end
--- conflicting_objectives/spec/subword_finder_spec.rb
+++ conflicting_objectives/spec/subword_finder_spec.rb
@@ -6,9 +6,47 @@ describe SubWordFinder do
     @data_dir = File.expand_path('../../data', __FILE__)
   end
 
-  describe '.load_file'
-    before { @subword_finder.load_file(File.join(@data_dir, 'fixtures1.txt')) }
+  describe '.load_file' do
+    before(:all) { @subword_finder.load_file(File.join(@data_dir, 'fixtures1.txt')) }
     it 'open the file, load the words and return Array' do
       expect(@subword_finder.words).to be_an(Array)
+    end
   end
+
+  describe '.find' do
+    before(:all) do
+      @subword_finder.load_file(File.join(@data_dir, 'fixtures1.txt'))
+      @results = @subword_finder.find
+    end
+
+    context 'with a file loaded find words and return hash' do
+      it { expect(@results).to be_an(Hash) }
+    end
+
+    context 'with fixtures1 loaded' do
+      it 'find correct results' do
+        expect(@results).to eql({"albums"=>[["al", "bums"]], "barely"=>[["bar", "ely"]], "befoul"=>[["be", "foul"]], "convex"=>[["con", "vex"]], "hereby"=>[["here", "by"]], "jigsaw"=>[["jig", "saw"]], "tailor"=>[["tail", "or"]], "weaver"=>[["we", "aver"]], "letter"=>[], "albert"=>[]})
+      end
+    end
+
+    describe '.find' do
+      before(:all) do
+        @subword_finder.load_file(File.join(@data_dir, 'fixtures2.txt'))
+        @results = @subword_finder.find
+      end
+
+      context 'with a file loaded find words and return hash' do
+        it { expect(@results).to be_an(Hash) }
+      end
+
+      context 'with fixtures1 loaded' do
+        it 'find correct number of results = 56674' do
+          expect(@results.flatten.length).to eql(56674)
+        end
+      end
+
+    end
+
+  end
+
 end
